f536fda12508c366182cd03847279390970fc3be
Mehmet Emin Yavaş <mehmett_175@outlook.com>
2023-07-21 23:50:30 +0300

    startup cleaning

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -14,6 +14,7 @@ using Core.Utilities.Security.Jwt;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Localization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Versioning;
@@ -25,179 +26,184 @@ using Microsoft.IdentityModel.Tokens;
 using Swashbuckle.AspNetCore.SwaggerUI;
 using ConfigurationManager = Business.ConfigurationManager;
 
-namespace WebAPI
+namespace WebAPI;
+
+/// <summary>
+///
+/// </summary>
+public partial class Startup : BusinessStartup
 {
     /// <summary>
-    ///
+    /// Constructor of <c>Startup</c>
     /// </summary>
-    public partial class Startup : BusinessStartup
+    /// <param name="configuration"></param>
+    /// <param name="hostEnvironment"></param>
+    public Startup(IConfiguration configuration, IHostEnvironment hostEnvironment)
+        : base(configuration, hostEnvironment)
     {
-        /// <summary>
-        /// Constructor of <c>Startup</c>
-        /// </summary>
-        /// <param name="configuration"></param>
-        /// <param name="hostEnvironment"></param>
-        public Startup(IConfiguration configuration, IHostEnvironment hostEnvironment)
-            : base(configuration, hostEnvironment)
+    }
+
+
+    /// <summary>
+    /// This method gets called by the runtime. Use this method to add services to the container.
+    /// </summary>
+    /// <remarks>
+    /// It is common to all configurations and must be called. Aspnet core does not call this method because there are other methods.
+    /// </remarks>
+    /// <param name="services"></param>
+    public override void ConfigureServices(IServiceCollection services)
+    {
+        // Business katmanında olan dependency tanımlarının bir metot üzerinden buraya implemente edilmesi.
+
+        services.AddControllers()
+            .AddJsonOptions(options =>
+            {
+                options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+                options.JsonSerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull;
+            });
+
+        services.AddApiVersioning(v =>
         {
+            v.DefaultApiVersion = new ApiVersion(1, 0);
+            v.AssumeDefaultVersionWhenUnspecified = true;
+            v.ReportApiVersions = true;
+            v.ApiVersionReader = new HeaderApiVersionReader("x-dev-arch-version");
+        });
+
+        services.AddCors(options =>
+        {
+            options.AddPolicy(
+                "AllowOrigin",
+                builder => builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+        });
+
+        var tokenOptions = Configuration.GetSection("TokenOptions").Get<TokenOptions>();
+
+        services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidIssuer = tokenOptions.Issuer,
+                    ValidAudience = tokenOptions.Audience,
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = SecurityKeyHelper.CreateSecurityKey(tokenOptions.SecurityKey),
+                    ClockSkew = TimeSpan.Zero
+                };
+            });
+        services.AddSwaggerGen(c =>
+        {
+            c.IncludeXmlComments(Path.ChangeExtension(typeof(Startup).Assembly.Location, ".xml"));
+        });
+
+        services.AddTransient<FileLogger>();
+        services.AddTransient<PostgreSqlLogger>();
+        services.AddTransient<MsSqlLogger>();
+        services.AddScoped<IpControlAttribute>();
+
+        base.ConfigureServices(services);
+    }
+
+
+    /// <summary>
+    /// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    /// </summary>
+    /// <param name="app"></param>
+    /// <param name="env"></param>
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        // VERY IMPORTANT. Since we removed the build from AddDependencyResolvers, let's set the Service provider manually.
+        // By the way, we can construct with DI by taking type to avoid calling static methods in aspects.
+        ServiceTool.ServiceProvider = app.ApplicationServices;
+
+
+        var configurationManager = app.ApplicationServices.GetService<ConfigurationManager>();
+        switch (configurationManager.Mode)
+        {
+            case ApplicationMode.Development:
+                _ = app.UseDbFakeDataCreator();
+                break;
+
+            case ApplicationMode.Profiling:
+            case ApplicationMode.Staging:
+
+                break;
+            case ApplicationMode.Production:
+                break;
         }
 
+        app.UseDeveloperExceptionPage();
 
-        /// <summary>
-        /// This method gets called by the runtime. Use this method to add services to the container.
-        /// </summary>
-        /// <remarks>
-        /// It is common to all configurations and must be called. Aspnet core does not call this method because there are other methods.
-        /// </remarks>
-        /// <param name="services"></param>
-        public override void ConfigureServices(IServiceCollection services)
+        app.ConfigureCustomExceptionMiddleware();
+
+        _ = app.UseDbOperationClaimCreator();
+
+        if (!env.IsProduction())
         {
-            // Business katmanında olan dependency tanımlarının bir metot üzerinden buraya implemente edilmesi.
+            app.UseSwagger();
 
-            services.AddControllers()
-                .AddJsonOptions(options =>
-                {
-                    options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
-                    options.JsonSerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull;
-                });
-
-            services.AddApiVersioning(v =>
+            app.UseSwaggerUI(c =>
             {
-                v.DefaultApiVersion = new ApiVersion(1, 0);
-                v.AssumeDefaultVersionWhenUnspecified = true;
-                v.ReportApiVersions = true;
-                v.ApiVersionReader = new HeaderApiVersionReader("x-dev-arch-version");
+                c.SwaggerEndpoint("v1/swagger.json", "DevArchitecture");
+                c.DocExpansion(DocExpansion.None);
             });
-
-            services.AddCors(options =>
-            {
-                options.AddPolicy(
-                    "AllowOrigin",
-                    builder => builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
-            });
-
-            var tokenOptions = Configuration.GetSection("TokenOptions").Get<TokenOptions>();
-
-            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-                .AddJwtBearer(options =>
-                {
-                    options.TokenValidationParameters = new TokenValidationParameters
-                    {
-                        ValidateIssuer = true,
-                        ValidateAudience = true,
-                        ValidateLifetime = true,
-                        ValidIssuer = tokenOptions.Issuer,
-                        ValidAudience = tokenOptions.Audience,
-                        ValidateIssuerSigningKey = true,
-                        IssuerSigningKey = SecurityKeyHelper.CreateSecurityKey(tokenOptions.SecurityKey),
-                        ClockSkew = TimeSpan.Zero
-                    };
-                });
-            services.AddSwaggerGen(c =>
-            {
-                c.IncludeXmlComments(Path.ChangeExtension(typeof(Startup).Assembly.Location, ".xml"));
-            });
-
-            services.AddTransient<FileLogger>();
-            services.AddTransient<PostgreSqlLogger>();
-            services.AddTransient<MsSqlLogger>();
-            services.AddScoped<IpControlAttribute>();
-
-            base.ConfigureServices(services);
         }
 
+        app.UseCors("AllowOrigin");
 
-        /// <summary>
-        /// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        /// </summary>
-        /// <param name="app"></param>
-        /// <param name="env"></param>
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        app.UseHttpsRedirection();
+
+        app.UseRouting();
+
+        app.UseAuthentication();
+
+        app.UseAuthorization();
+
+        // Make Turkish your default language. It shouldn't change according to the server.
+        app.UseRequestLocalization(new RequestLocalizationOptions
         {
-            // VERY IMPORTANT. Since we removed the build from AddDependencyResolvers, let's set the Service provider manually.
-            // By the way, we can construct with DI by taking type to avoid calling static methods in aspects.
-            ServiceTool.ServiceProvider = app.ApplicationServices;
+            DefaultRequestCulture = new RequestCulture("tr-TR"),
+        });
 
+        var cultureInfo = new CultureInfo("tr-TR");
+        cultureInfo.DateTimeFormat.ShortTimePattern = "HH:mm";
 
-            var configurationManager = app.ApplicationServices.GetService<ConfigurationManager>();
-            switch (configurationManager.Mode)
+        CultureInfo.DefaultThreadCurrentCulture = cultureInfo;
+        CultureInfo.DefaultThreadCurrentUICulture = cultureInfo;
+        app.UseStaticFiles(new StaticFileOptions
+        {
+            OnPrepareResponse = (context) =>
             {
-                case ApplicationMode.Development:
-                    _ = app.UseDbFakeDataCreator();
-                    break;
+                if (!IsAuthenticated(context.Context))
+                    throw new Exception(Messages.AuthorizationsDenied);
+            },
+            FileProvider = new PhysicalFileProvider(Path.Combine(env.ContentRootPath,
+                "wwwroot/images")),
+            RequestPath = "/images"
+        });
+        app.UseStaticFiles(new StaticFileOptions
+        {
+            FileProvider = new PhysicalFileProvider(Path.Combine(env.ContentRootPath,
+                "wwwroot/deneme")),
+            RequestPath = "/deneme"
+        });
+        app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
+    }
 
-                case ApplicationMode.Profiling:
-                case ApplicationMode.Staging:
+    private bool IsAuthenticated(HttpContext context)
+    {
+        var url = context.Request.Path.Value;
+        var claim = context.User.Claims.FirstOrDefault(x => x.Type.EndsWith("AvatarUrl"))?.Value;
 
-                    break;
-                case ApplicationMode.Production:
-                    break;
-            }
-
-            app.UseDeveloperExceptionPage();
-
-            app.ConfigureCustomExceptionMiddleware();
-
-            _ = app.UseDbOperationClaimCreator();
-
-            if (!env.IsProduction())
-            {
-                app.UseSwagger();
-
-                app.UseSwaggerUI(c =>
-                {
-                    c.SwaggerEndpoint("v1/swagger.json", "DevArchitecture");
-                    c.DocExpansion(DocExpansion.None);
-                });
-            }
-
-            app.UseCors("AllowOrigin");
-
-            app.UseHttpsRedirection();
-
-            app.UseRouting();
-
-            app.UseAuthentication();
-
-            app.UseAuthorization();
-
-            // Make Turkish your default language. It shouldn't change according to the server.
-            app.UseRequestLocalization(new RequestLocalizationOptions
-            {
-                DefaultRequestCulture = new RequestCulture("tr-TR"),
-            });
-
-            var cultureInfo = new CultureInfo("tr-TR");
-            cultureInfo.DateTimeFormat.ShortTimePattern = "HH:mm";
-
-            CultureInfo.DefaultThreadCurrentCulture = cultureInfo;
-            CultureInfo.DefaultThreadCurrentUICulture = cultureInfo;
-            app.UseStaticFiles(new StaticFileOptions
-            {
-                OnPrepareResponse = (context) =>
-                {
-                    var getUrl = context.Context.Request.Path.Value;
-                    var avatarUrl = context.Context.User.Claims.FirstOrDefault(x => x.Type.EndsWith("AvatarUrl"))
-                        ?.Value;
-                    if (context.Context.User.Identity is null ||
-                        !context.Context.User.Identity.IsAuthenticated ||
-                        context.Context.User.Claims.FirstOrDefault(x => x.Type.EndsWith("AvatarUrl"))?.Value is null ||
-                        !getUrl.EndsWith(
-                            context.Context.User.Claims.FirstOrDefault(x => x.Type.EndsWith("AvatarUrl"))?.Value)
-                       )
-                        throw new Exception(Messages.AuthorizationsDenied);
-                },
-                FileProvider = new PhysicalFileProvider(Path.Combine(env.ContentRootPath,
-                    "wwwroot/images")),
-                RequestPath = "/images"
-            });
-            app.UseStaticFiles(new StaticFileOptions
-            {
-                FileProvider = new PhysicalFileProvider(Path.Combine(env.ContentRootPath,
-                    "wwwroot/deneme")),
-                RequestPath = "/deneme"
-            });
-            app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
-        }
+        if (url != null &&
+            (context.User.Identity is null ||
+             !context.User.Identity.IsAuthenticated ||
+             claim is null ||
+             !url.EndsWith(claim)))
+            return false;
+        return true;
     }
 }
